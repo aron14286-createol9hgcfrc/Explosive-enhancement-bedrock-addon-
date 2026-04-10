895260ea9a9626a687df792350ab8d8e7e861ea6
aron14286-createol9hgcfrc <aron14286@antonian.org>
2026-04-10 12:37:53 -0500

    Update main.js

--- ExplosiveEnhancementBP/scripts/main.js
+++ ExplosiveEnhancementBP/scripts/main.js
@@ -1,48 +1,38 @@
 import { world } from "@minecraft/server";
 
 world.beforeEvents.explosion.subscribe((event) => {
-  const pos = source.location
-  const dim = source.dimension;
+  const { source, dimension } = event;
 
-  system.run(() =>{
-    const block = dim.getBlock(pos);
-    const inWater = block?.typeId.includes("water");
+  if (!source) return; // in case explosion has no source
 
-    if (inWater){
-      spawnUnderwaterEffects(dim, pos);
-    } else {
-      spawnNormalEffects(dim, pos);
-    }
-  });
-});
+  const pos = source.location;
+  const blockPos = {
+    x: Math.floor(pos.x),
+    y: Math.floor(pos.y),
+    z: Math.floor(pos.z),
   };
-  
-  const blockPos = { 
-    x: Math.floor(pos.x), 
-    y: Math.floor(pos.y), 
-    z: Math.floor(pos.z) 
-  };
-  const block = dim.getBlock(blockPos);
-  
-  const inWater = block?.typeId === "minecraft:water" || 
-                  block?.typeId === "minecraft:flowing_water";
-  
+
+  const block = dimension.getBlock(blockPos);
+  const inWater =
+    block?.typeId === "minecraft:water" ||
+    block?.typeId === "minecraft:flowing_water";
+
   if (inWater) {
-    spawnUnderwaterEffects(dim, pos);
+    spawnUnderwaterEffects(dimension, pos);
   } else {
-    spawnNormalEffects(dim, pos);
+    spawnNormalEffects(dimension, pos);
   }
 });
 
 function spawnNormalEffects(dim, pos) {
   const particlePos = { x: pos.x, y: pos.y + 0.5, z: pos.z };
-  
+
   // Primary explosion effects
   dim.spawnParticle("ee:blastwave", particlePos);
   dim.spawnParticle("ee:fireball", particlePos);
   dim.spawnParticle("ee:spark", pos);
   dim.spawnParticle("ee:smoke", pos);
-  
+
   // Directional smoke plumes
   dim.spawnParticle("ee:smoke", { x: pos.x + 0.75, y: pos.y + 0.5, z: pos.z });
   dim.spawnParticle("ee:smoke", { x: pos.x - 0.75, y: pos.y + 0.5, z: pos.z });
@@ -51,5 +41,6 @@ function spawnNormalEffects(dim, pos) {
 }
 
 function spawnUnderwaterEffects(dim, pos) {
-  dim.spawnParticle("ee:underwater_blast", { x: pos.x, y: pos.y + 
-                                            
+  dim.spawnParticle("ee:underwater_blast", { x: pos.x, y: pos.y + 0.5, z: pos.z });
+  // add any other underwater particles here
+}
